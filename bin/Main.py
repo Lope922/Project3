59010ed0303126a5a5d049929eb7ab92a3b17fec
CaLs_Rig <Lope922@gmail.com>
2016-03-01 01:43:47 -0600

    Got data back in a workable format now i just need to get it into Visual Studio

--- bin/Main.py
+++ bin/Main.py
@@ -1,181 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 import os.path
-
-
-def initialize_directory():
-    if os.path.isdir("Response_Info"):
-        print("we are good the path to store the response info in is made.")
-        open("C:/Users/CaLs_Rig/PycharmProjects/Project3/Program/Response_info/floodinfo.txt", mode="w+")
-    else:
-        os.mkdir("Response_info")
+import Program.Part1 as Weather
+import Program.bin.Part2 as PostalCode
 
 
 
-def get_basic_flood_info():
-
-# create request to the floodsafety website , reads response in html5
+def Main():
     try:
-        #generate the reqeust
-        r = requests.get("http://www.floodsafety.noaa.gov/", "html5lib")
+        # request information from National Weather Service
+        response = Weather.request_basic_weather_info()
 
-        #let make some soup
-        soup = BeautifulSoup(r.content, "html5lib")
+        # if response is not nothing
+        if response != None:
+            Weather.save_warning_info(response)
+            #todo here is where i would like to insert the break/check for call back to retrieve input from other app
 
-        #narrow down the tables
-        tables = soup.select("tr")
+            #read zip code file
+            try:
+                user_zip_input = PostalCode.get_zipcode_response()
+                PostalCode.zip_write_to_file(user_zip_input)
+                zipResponse = PostalCode.make_zip_request(user_zip_input)
 
-        # for testing purposes
-        #print(tables[1].find("p"))
-
-        # grab the items in index of 1 and find ones that start with p
-        info = tables[1].find("p")
-
-        #todo remove <p> format from the text
-
-        #flood details to be displayed in visual studio app stored here
-        Flood_deets = str(info)
+                fileWriter = open("~user\zip_code_response.html", mode="r")
+                PostalCodeList = PostalCode.read_table_data(zipResponse,fileWriter)
+                fileWriter.write(PostalCodeList)
 
 
-        return Flood_deets
-    #try and catch some errors not all yet
-    except requests.HTTPError as bad_request:
-        print("there as an error trying to make the web request. Check you connection and try again." + bad_request)
+                relPath = os.path.relpath("/Project3/Program/Response_info/", path="~user")
+                print("Here is a relative path " + relPath)
+
+
+            except os.error.errno:
+                    print("File could not be found. Try and enter your response again")
+
+        else:
+            print("Did not receive a response. Check your connection and try again.")
+
     except Exception as ex:
-        print("There has been an error. Source may have changed its layout, check source code\nMessage details" + ex)
-
-# todo strip the hypertext format from reply
-# store this either into a file or a variable to write onto a form.
-
-
-# pass in the response from the web to save to a file to display in another program.
-def record_flood_info(responseText):
-    #todo write to a file
-    save_path = "C:/Users\CaLs_Rig/PycharmProjects/Project3/Program/Response_info/"
-    file_name = "floodinfo.txt"
-    save = open(save_path + file_name, "a")
-    info = responseText
-    # convert its contents to string fromat
-    save.write(str(info))
-    # close the file so that other resources can use it
-    save.close()
-
-    #todo read from a file to make the next request using the zip provided through a different program.
-
-
-# all of this is done in HTML
-def read_zipcode_response():
-
-    zipinfo = (open('C:/Users/CaLs_Rig/PycharmProjects/Project3/Program/Response_info/users_zip_code.txt', 'r'))
-
-    # read the first line from the file that contains the zip
-    a = zipinfo.readline()
-    #print("here is the zipinfo " +a)
-    zipinfo.close()
-    return a
-
-
-#outer html for zip tag <a href="/zip-code/63139/zip-code-63139.asp">63139</a>
-# pass in the zip code retrieved from file
-#todo rewrite and split into two methods , one that makes the request and another that writes the request
-
-# needs the file location/name passed in for testing purposes only
-#Only need to run this when the zip changed
-def zip_write_to_file():
-    htmldata = open("zip_code_response.html", mode="w+", buffering=1, encoding="UTF-8")
-
-    htmldata.write(str(zip_response.text))
-    htmldata.close()
-    f = open("zip_code_response.html","r")
-    # making soup from file contents instead of reduce server request narrowin query
-    file_soup = BeautifulSoup(f, "html5lib")
-
-    print(file_soup.prettify())
-
-def read_from_file():
+        print("Error info " + str(ex))
 
 
 
 
 
+app = Main()
 
-def make_zip_request(zip_from_file):
-    try:
-        urlforZip = 'http://www.zip-codes.com/zip-code-radius-finder.asp?zipMilesLow=0&zipMilesHigh=5&zip1={0}&Submit=Search'.format(zip_from_file)
-
-        # make the request and store the response
-        zip_response = requests.get(urlforZip)
-
-        # turn it into soup #todo NOTE DO NOT DELETE ZIP SOUP changing to read from file
-        zip_soup = BeautifulSoup(zip_response.content, "html5lib")
-        # started with this
-        # achunk = zip_soup.select("td.a")
-       # zip_soup.find
-       # achunk = zip_soup.find_all("td.a", class_="a",attrs="a href=",recursive=True) # todo modify this to narrow down results.
-        #bchunk = zip_soup.find_all("td", class_="a", attrs="href", recursive=True)
-        #print("Here is be chunk " + str(bchunk))# todo modify this to narrow down results.
-
-        # if i can strip its contents i can search using it as a regular expression
-        #placeIwant = bchunk[1].find_all("a")
+app
 
 
-
-        # print("\n\n\n")
-        # for each_spot in bchunk:
-        #     print(str(each_spot))
-
-        p#rint("\n\n this is the chunk i sliced " + str(placeIwant))
-
-       # print("Here is a chunk" + str(achunk))
-       # print("_______________-----------------____________" + str(bchunk))
-
-        # website to possibly use and make use of zipcode information. http://www.zip-codes.com/zip-code-radius-finder.asp
-    except Exception as ex:
-        print("error trying to make web request for zip code details." + str(ex))
-
-
-# todo add some sort of trigger to initiate the get zip response method. IF FILE is not empty
-# todo look into async to see if the two programs need to be setup to communicate with eachother.
-# todo write a method that check the file size of the zip code response before it runs read zip code response
-
-# this requires program to have access and import os.path
-def remove_old_files():
-    os.rmdir("C:/Users/CaLs_Rig/PycharmProjects/Project3/Program/Response_info")
-
-
-# this method checks that a file has been made in Visual studio, and tries creates a request for the zip codes within a
-# given area
-def chk_file():
-    if os.path.isfile('C:/Users/CaLs_Rig/PycharmProjects/Project3/Program/Response_info/users_zip_code.txt'):
-    #if os.path.isfile('/Program/Response_info/users_zip_code.txt'):
-        print('i have found the file')
-    # may want to check length
-    #    print("The file contains some bytes")
-    #if the path exists return true
-        True
-
-    else:
-        print("file could not be found or has not been created.")
-    #need to double check this logic
-        False
-
-# ----------------  program stats here -----------------
-initialize_directory()
-
-# create the flood info from national weather service website
-reply = get_basic_flood_info()
-
-# save the info to a file to use later
-record_flood_info(reply)
-#if chk_file():
-     # if there are contents in the file then read them , and make request for list of zips , and return website
-users_zip = read_zipcode_response()
-make_zip_request(users_zip)
-if reply is None:
-      print("There was an error trying to retrieve the flood details")
-else:
-      print("Could not locate the proper file to process")
-print("program will now exit")
-# this isn't working yet, but not major
-#remove_old_files()
-exit(0)
--- bin/Part2.py
+++ bin/Part2.py
@@ -1,62 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
 
+class PostalCode:
 
-# all of this is done in HTML
-def read_zipcode_response():
+    def __init__(self):
+        return self
 
-    zipinfo = (open('users_zip_code.txt', 'r'))
-    # read the first line from the file that contains the zip
+#does return zip code so need to assign a value equal to this
+''' :parameter set equal to something '''
+def get_zipcode_response():
+    #todo adjust this path as well
+    zipinfo = (open('C:/Users/CaLs_Rig/PycharmProjects/Project3/Program/Response_info/users_zip_code.txt', 'r'))
+
+    # read the first line from the file that contains the zip that's all we need
     a = zipinfo.readline()
     #print("here is the zipinfo " +a)
+    zipinfo.close()
     return a
 
-# pass in the zip code retrieved from file
-def make_zip_request(zip_from_file):
-    try :
-        urlforZip = 'http://www.zip-codes.com/zip-code-radius-finder.asp?zipMilesLow=0&zipMilesHigh=10&zip1={0}&Submit=Search'.format(zip_from_file)
-        #print("here is the url to test by clicking on it " + urlforZip)
+# pass in the zip code retrieved from file needs the file location/name passed in for testing purposes only
+#Only need to run this when the zip changed
+def zip_write_to_file(zipResponse):
+    zip_response = zipResponse
+    htmldata = open("zip_code_response.html", mode="w+", buffering=1, encoding="UTF-8")
 
+    htmldata.write(str(zip_response.text))
+    htmldata.close()
+
+
+def read_from_file(zip_response):
+    f = open("zip_code_response.html","r")
+    # making soup from file contents instead of reduce server request narrowing query
+    file_soup = BeautifulSoup(f, "html5lib")
+
+    print(file_soup.prettify())
+
+def make_zip_request(zip_from_file):
+    try:
+        urlforZip = 'http://www.zip-codes.com/zip-code-radius-finder.asp?zipMilesLow=0&zipMilesHigh=5&zip1={0}&Submit=Search'.format(zip_from_file)
         # make the request and store the response
         zip_response = requests.get(urlforZip)
+        return zip_response
 
-        # turn it into soup
-        zip_soup = BeautifulSoup(zip_response.content, "html5lib")
-        #zip_soup.prettify()
-       # z = zip_soup.find_all("td", "a", limit=10)
-        # before it's turned into beautify soup print to see what it looks like
-       # print(z)
-
-
-        smallerChunk = zip_soup.find("a", {'href = '})
-        print(str(smallerChunk))
-
-        #regex =  zip_soup.attribselect_re("<td class = a><a href=>")
-        #print(str(regex))
-        #chunk_tag = zip_soup.find("<td class = a><a href=>")
-        #print("the new style : " + str(chunk_tag))
-
-
-        # from the response read the list of zips and add them to a list of radius' to alert
-
-        #todo trying to find all bs and their hrefs in the zipcode response
-        # read to the file that visual studio used to record the users input and generate a request for some new info based on the request.
-
-#        zip = 0
-
-        # open a file in the given path with the directory open it with the read only privleges.
-        #f = open('workfile', 'r')
-
-
-        # website to possibly use and make use of zipcode information. http://www.zip-codes.com/zip-code-radius-finder.asp
     except Exception as ex:
-        print("error trying to make web request for zip code details." + ex)
+        print("error trying to make web request for zip code details." + str(ex))
 
+# this method reads the current file instead of the response as originally programmed
+#todo this is the test data
+def read_table_data():
+    f = open("C:/Users\CaLs_Rig\PycharmProjects\Project3\Program\zip_code_response.html", mode="r")
+    newSoup = BeautifulSoup(f, "html5lib")
+    f.close()
 
+    zip_warning_file = open("ziplistWarning.txt",mode="w+")
+#print(newSoup.prettify())
 
-#todo add some sort of trigger to initiate the get zip response method. IF FILE is not empty
-#todo if file is empty halt the program and exit. Display a message letting the user know what went wrong.
+# extracts the text i want but there are so many extra strings still need to narrow it down
+# for link in newSoup.find_all("a"):
+#     print(link.get_text("href"))
+#     #print(link.get("href"))
 
+''' :param BeautifulSoup object, and filewriter filewriter should contain an already opened directory and be able to write to the file'''
+def writePostal_codes_to_file(ZipSoup, fileWriter):
 
-#todo look into async to see if the two programs need to be setup to communicate with eachother.
-
+    for lin in ZipSoup.find_all(name="td", class_="a", attrs="href", recursive=True):
+        a = " "
+        a += (lin.get_text("href")) + " "
+        fileWriter.writelines(a)
+    return a
+    ## print(lin.get_text("href"))
