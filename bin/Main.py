3b8a6ed1849fa1cf27c4d476ee93a42d446716b5
Lope <Lope922@gmail.com>
2016-03-01 21:34:29 -0600

    Have it working , but butchered Main and Part 2 in the process. Need to go back and refactor it.

--- bin/Main.py
+++ bin/Main.py
@@ -5,10 +5,6 @@ import Part1 as Weather
 import bin.Part2 as PostalCode
 import FileManager.FileManagerClass as fm
 
-
-
-
-
 def Main():
 
     an_easy_path = "C:/Users/Lope/PycharmProjects/Project3/Seperator/"
@@ -25,17 +21,38 @@ def Main():
         # read zip code file
         # todo if no such file exists halt for 6 or so seconds and wait for that process to complete
         # get user input from first form
-        user_zip_input = PostalCode.get_zipcode_response()
+        thezip = PostalCode.get_zipcode_response()
+        # get the zip from the file
+        #PostalCode.make_zip_request(thezip)
+
         # request their info
-        PostalCode.zip_write_to_file(user_zip_input)
-        zipResponse = PostalCode.make_zip_request(user_zip_input)
-        # fileWriter = open("C:/Users/Lope/PycharmProjects/Project3/Seperator/zip_code_response.html", mode="r")
-        # fileWriter.write(zipResponse)
+        zipResponse = PostalCode.make_zip_request(thezip)
+        #PostalCode.zip_write_to_file(zipResponse)
+        #todo extract the postal code list info
+        PostalCode.read_table_data(zipResponse)
+        data = ""
+        list_of_zip_file_location = open("C:/Users/Lope/PycharmProjects/Project3/Seperator/zipList.txt", mode="w")
+        for lin in zipResponse.find_all(name="td", class_="a", attrs="href", recursive=True):
 
-        PostalCodeList = PostalCode.read_table_data()
-        lastfile = open("C:/users/lope/pycharmProjects/Project3/Seperator/zipList.txt", mode="w+")
-        lastfile.write(PostalCodeList.text)
+            data += (lin.get_text("href")) + " "
+            #print(data)
+        list_of_zip_file_location.writelines(data)
+        list_of_zip_file_location.close()
 
+        # THIS WORKS , BUT TRYING TO PUT IN ITS PROPER PLACE
+        # data = ""
+        # list_of_zip_file_location = open("C:/Users/Lope/PycharmProjects/Project3/Seperator/zipList.txt", mode="w")
+        # for lin in zipResponse.find_all(name="td", class_="a", attrs="href", recursive=True):
+        #
+        #     data += (lin.get_text("href")) + " "
+        #     print(data)
+        # list_of_zip_file_location.writelines((data))
+        # list_of_zip_file_location.close()
+
+
+
+        # lastfile.write(PostalCodeList.text)
+        #PostalCode.writePostal_codes_to_file(PostalCodeList, lastfile)
     #         except os.error.errno:
     #                 print("File could not be found. Try and enter your response again")
     #
--- bin/Part2.py
+++ bin/Part2.py
@@ -7,7 +7,9 @@ class PostalCode:
         return self
 an_easy_path = "C:/Users/Lope/PycharmProjects/Project3/Seperator/"
 #does return zip code so need to assign a value equal to this
-''' :parameter set equal to something '''
+
+
+# set returns a zip so set it equal to a variable
 def get_zipcode_response():
     #todo adjust this path as well
     zipinfo = (open(an_easy_path + 'users_zip_code.txt', 'r'))
@@ -20,61 +22,75 @@ def get_zipcode_response():
 
 # pass in the zip code retrieved from file needs the file location/name passed in for testing purposes only
 #Only need to run this when the zip changed
-def zip_write_to_file(zipResponse):
+# def zip_write_to_file(zipResponse):
+#
+#     #zip_warning_file = open("ziplistWarning.txt", mode="w+")
+#     zip_response = zipResponse
+#     htmldata = open(an_easy_path +"zip_code_response.html", mode="w+", buffering=1, encoding="UTF-8")
+#
+#     htmldata.write(str(zip_response))
+#     htmldata.close()
 
-    #zip_warning_file = open("ziplistWarning.txt", mode="w+")
-    zip_response = zipResponse
-    htmldata = open(an_easy_path +"zip_code_response.html", mode="w+", buffering=1, encoding="UTF-8")
 
-    htmldata.write(str(zip_response))
-    htmldata.close()
+# def read_from_file(zip_response):
+#     f = open("zip_code_response.html","r")
+#     # making soup from file contents instead of reduce server request narrowing query
+#    # file_soup = BeautifulSoup(f, "html5lib")
+#
+#   #  print(file_soup.prettify())
+# #todo see why i am getting the response 200 error
 
-
-def read_from_file(zip_response):
-    f = open("zip_code_response.html","r")
-    # making soup from file contents instead of reduce server request narrowing query
-    file_soup = BeautifulSoup(f, "html5lib")
-
-    print(file_soup.prettify())
 
 def make_zip_request(zip_from_file):
     try:
         urlforZip = 'http://www.zip-codes.com/zip-code-radius-finder.asp?zipMilesLow=0&zipMilesHigh=5&zip1={0}&Submit=Search'.format(zip_from_file)
         # make the request and store the response
         zip_response = requests.get(urlforZip)
-        return zip_response
+        soup = BeautifulSoup(zip_response.content,"html5lib")
+        return soup
 
     except Exception as ex:
         print("error trying to make web request for zip code details." + str(ex))
 
 # this method reads the current file instead of the response as originally programmed
 #todo this is the test data
-def read_table_data(): # and make some soup out of it
+def read_table_data(soup): # and make some soup out of it
     try:
-        f = open("C:/Users/Lope/PycharmProjects/Project3/Seperator/zip_code_response.html", mode="r")
-        # make some new soup for our application
-        newSoup = BeautifulSoup(f, "html5lib")
-        f.close()
-        return newSoup
+       #  f = open("C:/Users/Lope/PycharmProjects/Project3/Seperator/zip_code_response.html", mode="r")
+       #  # make some new soup for our application
+       #  newSoup = BeautifulSoup(f, "html5lib")
+       # # print(newSoup.prettify())
+       #  f.close()
+       #  # create the open and write file command
+        list_of_zip_file_location = open("C:/Users/Lope/PycharmProjects/Project3/Seperator/zipList.txt", mode="w")
+       #  # writ the zip codes to a file using the method below to extract data
+
+        data = ""
+        for lin in soup.find_all(name="td", class_="a", attrs="href", recursive=True):
+            data += (lin.get_text("href")) + " "
+
+        list_of_zip_file_location.writelines((data))
+        list_of_zip_file_location.close()
+
+
+        #writePostal_codes_to_file(zipresponse_content, list_of_zip_file_location)
+       # return newSoup
 
     except IOError as ioerror:
         print("Problem trying to read in weather information" + str(ioerror))
 
 
-
-#print(newSoup.prettify())
-
-# extracts the text i want but there are so many extra strings still need to narrow it down
-# for link in newSoup.find_all("a"):
-#     print(link.get_text("href"))
-#     #print(link.get("href"))
-
 ''' :param BeautifulSoup object, and filewriter filewriter should contain an already opened directory and be able to write to the file'''
 def writePostal_codes_to_file(ZipSoup, fileWriter):
-
-    for lin in ZipSoup.find_all(name="td", class_="a", attrs="href", recursive=True):
-        a = " "
-        a += (lin.get_text("href")) + " "
-        fileWriter.writelines(a)
-    return a
+    #zipdata = BeautifulSoup(ZipSoup, "html5lib")
+    #fileWriter2 = open("C:/users/lope/pycharmProjects/Project3/Seperator/zipList.txt", mode="a")
+    # zs = BeautifulSoup(ZipSoup.contents,"html5lib")
+    data = ""
+    # for lin in ZipSoup.find_all(name="td", class_="a", attrs="href", recursive=True):
+    #
+    #     data += (lin.get_text("href")) + " "
+    #     print(data)
+    # fileWriter.writelines((data))
+    # fileWriter.close()
+    # #return data
     ## print(lin.get_text("href"))
